490153aab9326cfe2dcda832773bb3a3b9179987
Shaurya Singh <shauryasingh0302@icloud.com>
2025-09-26 01:56:07 +0530

    upload project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, ()=>{
+    console.log("Server is running on http://localhost:3000");
+});
+
